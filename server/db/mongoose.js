86969e722f7ec68984063b6bce8f0c00ffa7d71f
Christian Hoare <christian@Christians-MacBook-Pro.local>
2019-03-24 21:03:57 +0000

    Updating user access

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@
 var mongoose = require('mongoose');
 // set mongoose promise.
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 module.exportes = {mongoose};
--- server/server.js
+++ server/server.js
@@ -27,10 +27,34 @@ var app = express();
 //Adding in body parser as middleware to express()
 
 app.use(bodyParser.json());
-app.use(cors);
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+//app.use(cors);
 
 //Todo routes
 
+//Test server is running 
+
+app.get('/',(req,res) => {
+  res.send("The server is fine")
+});
 
 //add a todo
 app.post('/todos', authenticate, (req, res) =>{
@@ -147,7 +171,7 @@ app.post('/users/login', (req,res) => {
       res.header('x-auth', token).send(user);
       });
     }).catch((e) =>{
-      res.status(400).send();
+      res.status(400).send(e);
     });
 });
 
